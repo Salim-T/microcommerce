535044c1aa09c69012b647873d7e29b5c54f6cee
Salim-T <tizi_salim@yahoo.fr>
2025-07-02 20:00:07 +0200

    add tests

--- src/test/java/com/ecommerce/microcommerce/MicrocommerceApplicationTests.java
+++ src/test/java/com/ecommerce/microcommerce/MicrocommerceApplicationTests.java
@@ -2,12 +2,15 @@ package com.ecommerce.microcommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MicrocommerceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Ce test vérifie que le contexte Spring se charge correctement
 	}
 
 }
